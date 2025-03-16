bb4d2cb1a4f61ea951269ce32fcd6bf8754123ff
Alessandro Sottile <sottile.alessandro10@gmail.com>
2025-03-16 12:01:57 +0100

    update

--- app.py
+++ app.py
@@ -5,12 +5,18 @@ import asyncio
 from torrentp import TorrentDownloader
 import os
 import re
+import asyncio
+import concurrent.futures
 
 
 torrents = py1337x.Py1337x()
 
 app = Flask(__name__)
 CORS(app)
+
+def download_in_background(magnet_link, download_path):
+    torrent_file = TorrentDownloader(magnet_link, download_path)
+    asyncio.run(torrent_file.start_download())
 
 @app.route("/search_torrent", methods=["POST"])
 def search_torrent():
@@ -37,6 +43,15 @@ def download_torrent():
     data = request.json
     magnet_link = data.get("magnet_link")
     category = data.get("category")  # "Film", "TV-Series", "Other"
+    short_name = data.get('short_name', '').strip()  # Aggiungi un default vuoto e rimuovi gli spazi
+
+    if not short_name:
+        match = re.search(r'dn=([^&]+)', magnet_link)
+        if not match:
+            return jsonify({"message": "Invalid magnet link format"}), 400
+        folder_name = match.group(1)
+    else:
+        folder_name = short_name
 
     if not magnet_link or not category:
         return jsonify({"message": "Missing required parameters"}), 400
@@ -47,26 +62,23 @@ def download_torrent():
         "TV-Series": "/Serie/",
         "Other": "/Other/"
     }
+    # category_paths = {
+    #     "Film": "Film/",
+    #     "TV-Series": "Serie/",
+    #     "Other": "Other/"
+    # }
 
     # Percorso per la categoria scelta
     category_path = category_paths.get(category)
 
-    # Estrazione della stringa tra 'dn=' e il primo '&'
-    match = re.search(r'dn=([^&]+)', magnet_link)
-    if not match:
-        return jsonify({"message": "Invalid magnet link format"}), 400
-
-    folder_name = match.group(1)
     download_path = os.path.join(category_path, folder_name)
 
     # Creazione della cartella se non esiste
     os.makedirs(download_path, exist_ok=True)
-
-    # Avvio del download
-    torrent_file = TorrentDownloader(magnet_link, download_path)
-    asyncio.run(torrent_file.start_download())
-
     print(f"Downloading {category} with magnet link: {magnet_link} to {download_path}")
+    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.submit(download_in_background, magnet_link, download_path)
 
     return jsonify({"message": f"{category} download started successfully!"}), 200
 
